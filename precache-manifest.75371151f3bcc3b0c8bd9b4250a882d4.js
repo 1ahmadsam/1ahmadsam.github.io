71be38add4d8831b501c3c65cd2450f7d7f8da2a
1ahmadsam <samiahmad360@hotmail.com>
2020-10-15 03:11:40 -0400

    redploying on different directory

---
+++ precache-manifest.75371151f3bcc3b0c8bd9b4250a882d4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8666830ed7ed067c83f52d27d1b68407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e39eb64f1b897c5c20",
+    "url": "/static/css/main.0fcf4d6c.chunk.css"
+  },
+  {
+    "revision": "df2e3788df056980bca7",
+    "url": "/static/js/2.30ce2cfd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.30ce2cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e39eb64f1b897c5c20",
+    "url": "/static/js/main.14a7a456.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "27467011d3a57779043517908cdacb7f",
+    "url": "/static/media/airbnb.27467011.jpg"
+  },
+  {
+    "revision": "3fa9524fce9393d5a8ab620521c6bcea",
+    "url": "/static/media/amazon.3fa9524f.jpg"
+  },
+  {
+    "revision": "1b3ea410a3020005e0b83a8b530a8678",
+    "url": "/static/media/mountain-morning.1b3ea410.jpg"
+  },
+  {
+    "revision": "3a7197bb3a3ada6c7fe1f1b7069986db",
+    "url": "/static/media/netflix.3a7197bb.jpg"
+  },
+  {
+    "revision": "112a1b9e2d693da309518fac34b2761e",
+    "url": "/static/media/reddit.112a1b9e.jpg"
+  },
+  {
+    "revision": "ff8d5e0c0b0bb8d92ffd37f9a906b1a5",
+    "url": "/static/media/sami.ff8d5e0c.jpg"
+  },
+  {
+    "revision": "2e7420500326dc255fdf14b0d5a06c4a",
+    "url": "/static/media/spotify.2e742050.jpg"
+  },
+  {
+    "revision": "f8d06c0f0cc7f1280aa668b1f4e8e186",
+    "url": "/static/media/twitter.f8d06c0f.jpg"
+  },
+  {
+    "revision": "89f5075567710789ba31788cce264d02",
+    "url": "/static/media/vacation.89f50755.jpg"
+  }
+]);
